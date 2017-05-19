d941b52a5dbd19a0eb8d9870caad70fbeb2ffb00
Melissa <melissa.pacheco.martinez@gmail.com>
2017-05-19 17:53:02 -0400

    pandas dsd css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
-var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+/*var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
 
 var fotos = document.getElementById("imagenes");
+var ekis = document.createElement("i")
+ekis.setAttribute("class", "fa fa-times")
+ekis.setAttribute("aria-hidden", "true")
+
 
 pictures.forEach(function(cv){
 	var imagen = document.createElement("img");
@@ -8,3 +12,12 @@ pictures.forEach(function(cv){
 	imagen.setAttribute("src", cv);
 	fotos.appendChild(imagen);
 });
+
+*/
+
+	/*<i class="fa fa-times" aria-hidden="true"></i>*/
+	var boton =	document.getElementById("mostrar");
+	boton.addEventListener("click", function(){
+		this.classList.toggle("texto2");
+		});
+	
